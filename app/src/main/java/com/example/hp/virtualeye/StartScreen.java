2066e8208b1d019a46a4f5c53e8fa9df16f34698
ankitmane11 <44810187+ankitmane11@users.noreply.github.com>
2019-09-30 21:40:48 +0530

    Version 1 ready

--- app/src/main/java/com/example/hp/virtualeye/StartScreen.java
+++ app/src/main/java/com/example/hp/virtualeye/StartScreen.java
@@ -1,24 +1,34 @@
 package com.example.hp.virtualeye;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class StartScreen extends AppCompatActivity {
 
     Button objectTagging;
+    Button imageProcessing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
         objectTagging=findViewById(R.id.object_tagging);
+        imageProcessing=findViewById(R.id.image_processing);
         objectTagging.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(StartScreen.this,MainActivity.class);
                 startActivity(intent);
+            }
+        });
+        imageProcessing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Coming soon",Toast.LENGTH_SHORT).show();
             }
         });
     }
